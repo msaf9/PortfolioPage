704ce407b6adcc7858f99c13ff4044604ee73ca1
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-07-04 02:09:35 +0530

    scroll to the top function implementation

--- main.js
+++ main.js
@@ -1,37 +1,11 @@
-$(document).ready(function () {
-    $("#div1").click(function () {
-        $("#div2").removeClass("blue");
-        $("#div3").removeClass("blue");
-        $("#div4").removeClass("blue");
-        $("#div5").removeClass("blue");
-        $("#div1").addClass("blue");
-    });
-    $("#div2").click(function () {
-        $("#div1").removeClass("blue");
-        $("#div3").removeClass("blue");
-        $("#div4").removeClass("blue");
-        $("#div5").removeClass("blue");
-        $("#div2").addClass("blue");
-    });
-    $("#div3").click(function () {
-        $("#div1").removeClass("blue");
-        $("#div2").removeClass("blue");
-        $("#div4").removeClass("blue");
-        $("#div5").removeClass("blue");
-        $("#div3").addClass("blue");
-    });
-    $("#div4").click(function () {
-        $("#div1").removeClass("blue");
-        $("#div2").removeClass("blue");
-        $("#div3").removeClass("blue");
-        $("#div5").removeClass("blue");
-        $("#div4").addClass("blue");
-    });
-    $("#div5").click(function () {
-        $("#div1").removeClass("blue");
-        $("#div2").removeClass("blue");
-        $("#div3").removeClass("blue");
-        $("#div4").removeClass("blue");
-        $("#div5").addClass("blue");
-    });
-});
+/* Funtion for scrolling to top*/
+function someFunction() {
+    //In a snap
+    /* window.scrollTo(0, 0); */
+
+    // With some animation
+    /* $('html, body').animate({ scrollTop: 0 }, 'slow'); */
+
+    // Smooth scroll behaviour
+    window.scrollTo({ top: 0, behavior: `smooth` })
+}
