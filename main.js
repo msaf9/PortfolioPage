d66edd5896f18f5fecbd2bcc61354fbdf5e0164d
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-07-08 23:44:44 +0530

    js cleanup

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 /* Funtion for scrolling to top*/
 function someFunction() {
-    //In a snap
-    /* window.scrollTo(0, 0); */
-
-    // With some animation
-    /* $('html, body').animate({ scrollTop: 0 }, 'slow'); */
-
+    
     // Smooth scroll behaviour
     window.scrollTo({ top: 0, behavior: `smooth` })
 }
