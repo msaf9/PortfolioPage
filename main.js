2f24f3135d43773e954ff2bf938c7b31add7eba9
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-09-19 01:19:31 +0530

    format modified

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 /* Funtion for scrolling to top*/
 function someFunction() {
-    
-    // Smooth scroll behaviour
-    window.scrollTo({ top: 0, behavior: `smooth` })
+  // Smooth scroll behaviour
+  window.scrollTo({ top: 0, behavior: `smooth` });
 }
