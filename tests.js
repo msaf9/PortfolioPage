9eaeecd1530b009fda330f599579b0836e4a76c9
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-10-29 00:30:52 +0530

    WIP: Unit testing

---
+++ tests.js
@@ -0,0 +1,8 @@
+const chai = window.chai;
+const expect = chai.expect;
+
+describe("test the someFunction", () => {
+  it("should navigate to the top of the page when called", () => {
+    // expect(chai.scrollFunction).to.have.property("callCount", 1);
+  });
+});
