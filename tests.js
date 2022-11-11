408c1c6f06475f3bc8d495f9c83a913ec901dffd
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-11-11 16:00:56 +0530

    Configured prettier to improve code readability

--- tests.js
+++ tests.js
@@ -1,8 +1,8 @@
 const chai = window.chai;
 const expect = chai.expect;
 
-describe("test the someFunction", () => {
-  it("should navigate to the top of the page when called", () => {
+describe('test the someFunction', () => {
+  it('should navigate to the top of the page when called', () => {
     // expect(chai.TopScrollFunction).to.have.property("callCount", 1);
   });
 });
